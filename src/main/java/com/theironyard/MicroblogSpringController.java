e759d2792c0fa7a3681a531c6ef0de51692a4e1a
ryankielty <rpkielty@gmail.com>
2017-01-19 15:35:57 -0600

    Added delete button, should be finished with required/optional tasks

--- src/main/java/com/theironyard/MicroblogSpringController.java
+++ src/main/java/com/theironyard/MicroblogSpringController.java
@@ -13,32 +13,48 @@ import java.util.ArrayList;
  */
 @Controller
 public class MicroblogSpringController {
+
     public ArrayList<Message> messages = new ArrayList<>();
+
     @RequestMapping(path = "/", method = RequestMethod.GET)
     public String home(Model model, HttpSession session) {
+
         model.addAttribute("name", session.getAttribute("userName"));
         model.addAttribute("messages", messages);
+
         return "home";
     }
     @RequestMapping(path = "/login", method = RequestMethod.POST)
-    public String login(HttpSession session, String userName) {
-        session.setAttribute("userName", userName);
+    public String login(HttpSession session, String userName) throws Exception {
+
+        if (userName == null) {
+            throw new Exception("Enter Name");
+        } else {
+            session.setAttribute("userName", userName);
+        }
+
         return "redirect:/";
     }
     @RequestMapping(path = "/logout", method = RequestMethod.POST)
     public String logout(HttpSession session) {
+
         session.invalidate();
+
         return "redirect:/";
     }
     @RequestMapping(path = "/add-message", method = RequestMethod.POST)
     public String message(String addMessage) {
-        Message mess = new Message (messages.size() + 1, addMessage);
+
+        Message mess = new Message ((messages.size() + 1), addMessage);
         messages.add(mess);
+
         return "redirect:/";
     }
     @RequestMapping(path = "/delete-message", method = RequestMethod.POST)
     public String deleteMessage(int id) {
+
         messages.remove(id - 1);
+
         return "redirect:/";
     }
 }
